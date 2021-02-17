70cea81e895760be19e9c8a862c0751f5ada4183
Ewing Eng <eeng275@gmail.com>
2021-02-16 19:03:11 -0500

    Add project files.

---
+++ NoStamCosts.cs
@@ -0,0 +1,64 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using System.IO;
+using System.Reflection;
+
+namespace ValheimNoStam
+{
+
+    [BepInPlugin(GUID, MODNAME, VERSION)]
+    public class NoStamCosts : BaseUnityPlugin
+    {
+        #region[Declarations]
+
+        public const string
+            MODNAME = "NoStamCosts",
+            AUTHOR = "Basil",
+            GUID = AUTHOR + "_" + MODNAME,
+            VERSION = "0.1.0";
+
+        internal readonly ManualLogSource log;
+        internal readonly Harmony harmony;
+        internal readonly Assembly assembly;
+        public readonly string modFolder;
+
+        #endregion
+
+        #region Config
+
+        public static ConfigEntry<int> StaminaCostSetting;
+
+        #endregion
+
+        #region Initialize Config
+        
+        public void InitConfig()
+        {
+            StaminaCostSetting = Config.Bind(
+                "Stamina Cost Setting",
+                "StaminaCostSetting",
+                1,
+                "0 = Off, 1 = No stamina costs only when using the hoe or hammer, 2 = No stamina costs for everything in god mode only, 3 = No stamina costs at all for everything even when not in god mode"
+                );
+        }
+
+        #endregion
+
+        public NoStamCosts()
+        {
+            log = Logger;
+            harmony = new Harmony(GUID);
+            assembly = Assembly.GetExecutingAssembly();
+            modFolder = Path.GetDirectoryName(assembly.Location);
+        }
+
+        public void Awake()
+        {
+            InitConfig();
+            harmony.PatchAll(assembly);
+        }
+
+    }
+}
---
+++ Patches/NoStaminaCostPatch.cs
@@ -0,0 +1,39 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace ValheimNoStam.Patches
+{
+
+    [HarmonyPatch(typeof(Player), "UseStamina")]
+    static class NoStaminaCostPatch
+    {
+        static void Prefix(Player __instance, ref float v)
+        {   
+            switch (NoStamCosts.StaminaCostSetting.Value)
+            {
+                // Use stamina
+                case 0:
+                    return;
+                //  No stamina costs when using hammer or hoe
+                case 1:
+                    // Debug.Log(__instance.GetRightItem().m_shared.m_name);
+                    string itemName = __instance.GetRightItem().m_shared.m_name;
+                    if (itemName == "$item_hammer" || itemName == "$item_hoe")
+                        v = 0;
+                    return;
+                // No stamina costs in god mode only
+                case 2:
+                    if (__instance.InGodMode())
+                        v = 0;
+                    return;
+                // No stamina costs at all regardless of mode
+                case 3:
+                    v = 0;
+                    return;
+                default:
+                    return;
+            }
+        }
+    }
+
+}
